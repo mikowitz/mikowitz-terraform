14eeaf5b963ea3098ccf09c8bb4590b2aa027640
Michael Berkowitz <michael.berkowitz@gmail.com>
2018-03-02 15:11:56 -0500

    Remove "Michael" user

--- users.tf
+++ users.tf
@@ -2,8 +2,3 @@ resource "aws_iam_user" "michael_berkowitz" {
   name = "michael.berkowitz"
   force_destroy = true
 }
-
-resource "aws_iam_user" "michael" {
-  name = "Michael"
-  force_destroy = true
-}
