4a171f5f6da73fde7879a845004ec81ca83a096d
Michael Berkowitz <michael.berkowitz@gmail.com>
2018-10-11 13:16:19 -0400

    Set up LinuxAcademy users, groups, policies

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+## Admins
 resource "aws_iam_group" "admins" {
   name = "Admins"
 }
@@ -14,3 +15,33 @@ resource "aws_iam_group_policy_attachment" "admins_administrator_access" {
   group = "${aws_iam_group.admins.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+## Dev
+resource "aws_iam_group" "dev" {
+  name = "Dev"
+}
+
+resource "aws_iam_group_membership" "dev" {
+  name = "Dev"
+  users = [
+    "${aws_iam_user.donna.name}",
+    "${aws_iam_user.kunal.name}",
+    "${aws_iam_user.matt.name}"
+  ]
+  group = "${aws_iam_group.dev.name}"
+}
+
+resource "aws_iam_group_policy_attachment" "dev_s3_full_access" {
+  group = "${aws_iam_group.dev.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "dev_ec2_full_access" {
+  group = "${aws_iam_group.dev.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+resource "aws_iam_group_policy_attachment" "dev_rds_full_access" {
+  group = "${aws_iam_group.dev.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
--- users.tf
+++ users.tf
@@ -2,3 +2,18 @@ resource "aws_iam_user" "michael_berkowitz" {
   name = "michael.berkowitz"
   force_destroy = true
 }
+
+resource "aws_iam_user" "donna" {
+  name = "Donna"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "kunal" {
+  name = "Kunal"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "matt" {
+  name = "Matt"
+  force_destroy = true
+}
